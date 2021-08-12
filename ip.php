67b0f5d797d02d7af8d60bd31f9137df1e29fd99
Softgetix <softgetix@gmail.com>
2021-08-12 13:09:01 +0530

    Add ip file

---
+++ ip.php
@@ -0,0 +1,32 @@
+<?php 
+header('Content-Type: application/json');
+header('x-hello-world:test'); 
+
+$data['ip'] = get_client_ip();
+
+if(isset($_REQUEST['name']) && !empty($_REQUEST['name']))
+	$data['greeting'] = $_REQUEST['name'];
+
+echo json_encode($data);
+
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+?>
